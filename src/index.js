16ce20b25e2d293b95666c97e60c359cba4ebfbd
Amanda <amanda.natallie.2@gmail.com>
2022-02-04 04:20:50 -0300

    feat: user post n todos get/post/put api created

--- src/index.js
+++ src/index.js
@@ -1,40 +1,91 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount(req, res, next) {
+  const { username } = req.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!username) return res.status(400).json({ error: "User not found!" });
+
+  req.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
+// User create
+app.post("/users", (req, res) => {
+  const { name, username } = req.body;
+  const user = {
+    name,
+    username,
+    id: uuidv4(),
+    todos: [],
+  };
+
+  users.push(user);
+  console.log(users);
+  return res.status(201).send();
+});
+
+app.get("/todos", checksExistsUserAccount, (req, res) => {
+  const { user } = req;
+
+  return res.json(user.todos);
+});
+
+app.post("/todos", checksExistsUserAccount, (req, res) => {
+  const { title, deadline } = req.body;
+  const { user } = req;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: "2021-02-22T00:00:00.000Z",
+  };
+
+  user.todos.push(todo);
+
+  return res.status(201).json(todo);
+});
+
+app.put("/todos/:id", checksExistsUserAccount, (req, res) => {
+  const { id } = req.params;
+  const { title, deadline } = req.body;
+  const { user } = req;
+
+  const newArrayWithUpdatedTask = user.todos.map((todo) => {
+    if (todo.id === id) {
+      return {
+        ...todo,
+        title,
+        deadline: new Date(deadline),
+      };
+    }
+
+    return todo;
+  });
+
+  user.todos = newArrayWithUpdatedTask;
+
+  return res.status(201).send();
+});
+
+app.patch("/todos/:id/done", checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+app.delete("/todos/:id", checksExistsUserAccount, (req, res) => {
   // Complete aqui
 });
 
